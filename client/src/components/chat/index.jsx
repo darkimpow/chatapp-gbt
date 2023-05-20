e45aaee7da7ab0687afc28c94651851fb61922cf
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-20 15:21:47 -0400

    imported createmessageform within custommessageform directory

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useMultiChatLogic, MultiChatSocket,MultiChatWindow} from "react-chat-engine-advanced";
 import Header from "@/components/customHeader";
-
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 function Chat () {
     const chatProps = useMultiChatLogic(
         import.meta.env.VITE_PROJECT_ID,
@@ -15,7 +15,11 @@ function Chat () {
                 {...chatProps}
                 style={{height: "100vh"}}
                 renderChatHeader={(chat) => <Header chat={chat} /> }
-
+                renderMessageForm={(props) => {
+                  return(
+                      <StandardMessageForm props={props} activeChat={chatProps.chat} />
+                  )
+            }}
             />
         </div>
     );

e52971a2a99528db012cf668d8e2b0f4a71ed93f
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-20 14:42:52 -0400

    custom header directory file path within components

--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { ChatBubbleLeftRightIcon,PhoneIcon} from "@heroicons/react/24/solid"
+import {ChatBubbleLeftRightIcon, PhoneIcon} from "@heroicons/react/24/solid"
 
-function CustomHeader () {
+
+const CustomHeader = ({ chat }) =>  {
     return (
-        <div>CustomHeader</div>
+        <div className="chat-header">
+        <div className="flexbetween">
+            <ChatBubbleLeftRightIcon className="icon-chat"/>
+            <h3 className="header-text">{chat.title}</h3>
+        </div>
+        <div className="flexbetween">
+            <PhoneIcon className="icon-phone" />
+            <p className="header-text">{chat.description}</p>
+        </div>
+    </div>
     );
-}
+};
 
 export default CustomHeader;

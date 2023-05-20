faa089edd9f069b1ee385e5579da1322e3d80ed2
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-20 13:19:15 -0400

    created eslint.json file for react app npm package config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-
+import {useState} from "react";
+import {BrowserRouter ,Router,Routes, Navigate} from "react-router-dom";
 
 function App() {
-
-  return (
-      <div className="app">app</div>
-
-    )
+  return <div className="app">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App

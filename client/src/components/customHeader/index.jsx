935e6b58dfa7d88395b6bbbf19882d00f26f6ec2
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-20 14:16:42 -0400

    custom header directory file path within components

---
+++ client/src/components/customHeader/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ChatBubbleLeftRightIcon,PhoneIcon} from "@heroicons/react/24/solid"
+
+function CustomHeader () {
+    return (
+        <div>CustomHeader</div>
+    );
+}
+
+export default CustomHeader;
